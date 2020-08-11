105b499da048f792ca10e698de5eb6b760c27b79
Никита Целков <nikitacelkov@gmail.com>
2020-08-11 16:18:41 +0700

    Добавьте файлы проекта.

---
+++ ViewModels/LoginWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Models;
+using System;
+
+namespace ViewModels
+{
+    public class LoginWindowViewModel
+    {
+        public event Action OnClose;
+
+        public RelayCommand CloseWindow { get; set; }
+
+        public LoginWindowViewModel()
+        {
+            CloseWindow = new RelayCommand(CloseWindowMetod);
+        }
+
+        public void CloseWindowMetod(object param)
+        {
+            Console.WriteLine(param + "It is Working");
+            OnClose?.Invoke();
+        }
+    }
+}
---
+++ ViewModels/Navigation/NavigateViewModel.cs
@@ -0,0 +1,24 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels.Navigation
+{
+    public class NavigateViewModel : ViewModelBase
+    {
+        public NavigateViewModel()
+        {
+
+        }
+
+        public void Navigate(string url)
+        {
+            Messenger.Default.Send<NavigateArgs>(new NavigateArgs(url));
+        }
+    }
+
+}
